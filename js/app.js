ff395ecca16ba803f7f24175a499769f064a5510
habib-sc <habib97.cs@gmail.com>
2022-02-28 14:50:14 +0600

    fix responsive and added validation

--- js/app.js
+++ js/app.js
@@ -5,6 +5,11 @@ const getLocation = () => {
     const searchField = document.getElementById('search-field');
     const searchText = searchField.value;
 
+    if (searchText == '') {
+        alert('Please Enter a City Name!');
+        return;
+    }
+
     // Fetching location API 
     const locationApiUrl = `http://api.openweathermap.org/geo/1.0/direct?q=${searchText}&limit=5&appid=6894a6943ffac86b0dcf7a9083863dbe`;
 
@@ -34,10 +39,10 @@ const displayWeather = (weather, cityInfo) => {
     const weatherContainer = document.getElementById('weather-container');
     const div = document.createElement('div');
     div.innerHTML = `
-    <div class="relative block p-8 overflow-hidden border border-gray-100 rounded-lg mt-5" href="">
+    <div class="relative block p-8 overflow-hidden border border-gray-100 rounded-lg mt-10" href="">
         <span class="absolute inset-x-0 bottom-0 h-2  bg-gradient-to-r from-green-300 via-blue-500 to-purple-600"></span>
 
-        <div class="justify-between items-center sm:flex gap-4">
+        <div class="justify-between items-center sm:flex sm:gap-8 md:gap-4">
             <div>
                 <h4 class="text-2xl font-bold text-gray-600">
                     ${cityInfo.cityName}
@@ -48,16 +53,16 @@ const displayWeather = (weather, cityInfo) => {
                 </p>
             </div>
 
-            <div class="flex-shrink-0 hidden ml-3 sm:block">
+            <div>
                 <p class="mt-1 text-lg font-medium text-gray-600">
-                    Today <span class="text-xl"></span>
+                    Today's <span class="text-xl"></span>
                 </p>
                 <p class="mt-1 text-lg font-medium text-gray-600">
                     Temperature <span class="text-xl text-gray-900">${kelToCell(weather.main.temp_min)}º</span> / <span class="text-xl text-gray-900">${kelToCell(weather.main.temp_max)}º</span>
                 </p>
             </div>
 
-            <div class="flex-shrink-0 hidden ml-3 sm:block">
+            <div>
                 <p class="mt-1 text-lg font-medium text-gray-600">
                     Currently <span class="text-xl text-gray-900">${kelToCell(weather.main.temp)}º</span>
                 </p>
@@ -67,10 +72,9 @@ const displayWeather = (weather, cityInfo) => {
 
             </div>
 
-            <div class="flex-shrink-0 hidden ml-3 sm:block">
+            <div class="flex md:flex-col items-center">
                 <img class="object-cover w-16 h-16 rounded-lg shadow-sm" src="${weatherIconUrl}" />
                 <p class="mt-1 text-lg font-medium text-gray-600 text-center mt-2">${weather.weather[0].main}</p>
-
             </div>
 
         </div>
